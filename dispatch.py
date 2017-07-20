924a524f7d9b7afc93f81be13c8f7b19a58ddc38
George <gabolaev98@gmail.com>
2017-07-20 17:26:59 +0300

    Added more regular expressions so that the bot could perceive a more ordinary human speech. Also now he can tell you about himself.

--- dispatch.py
+++ dispatch.py
@@ -4,16 +4,13 @@ import dbUtils
 listOfUsers = dbUtils.getListOfUsers()
 
 
-# def sendToAll(text):
-    # bot.bot.send_message(43903450, text=text)
-    # for i in listOfUsers:
-    #     try:
-    #         bot.bot.send_message(i[2], text=text)
-    #         bot.logging(text='РАССЫЛКА:'+text)
-    #     except bot.telebot.apihelper.ApiException as ex:
-    #         bot.logging(text=ex)
-#
-#
-# sendToAll("""Обновление v1.3.1 (beta)
-#
-# ✅ Добавлена красивая :) инструкция. Для получения следует нажать кнопку "Как оно работает?" """)
+def sendToAll(text):
+    for i in listOfUsers:
+        try:
+            bot.bot.send_message(i[2], text=text, reply_markup=bot.main)
+            bot.logging(text='РАССЫЛКА:'+text)
+        except bot.telebot.apihelper.ApiException as ex:
+            bot.logging(text=ex)
+
+while(1):
+    sendToAll(input())
