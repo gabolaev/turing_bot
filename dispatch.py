7874bfa1850afd66ac885798de720b91e45ae058
George <gabolaev98@gmail.com>
2017-09-22 17:12:09 +0300

    little changes in dispatch

--- dispatch.py
+++ dispatch.py
@@ -2,18 +2,19 @@ import bot
 import dbUtils
 import time
 
-listOfUsers = dbUtils.getListOfUsers()
-def sendToAll(text):
-    for i in listOfUsers:
-        try:
-            bot.bot.send_message(i[2], "Кстати про разбор...")
-            time.sleep(1)
-            bot.bot.send_message(i[2], """Над решениями работали:\nГуев Тимур\nГуссаова Рената\nГутнова Дзерасса\nПлиева Мадина\nТасоева Эсма\nЧшиев Аслан""")
-            time.sleep(1)
-            bot.bot.send_message(i[2], "АНТИХАЙП")
-            bot.log.info("{} ({}) : Доставлено".format(i[1],i[2]))
-            time.sleep(2)
-        except bot.telebot.apihelper.ApiException as ex:
-            bot.log.error("{} ({}) : {}".format(i[1], i[2], ex))
+# listOfUsers = dbUtils.getListOfUsers()
+# def sendToAll(text):
+#     for i in listOfUsers:
+#         try:
+#             bot.bot.send_message(i[2], "Кстати про разбор...")
+#             time.sleep(1)
+#             bot.bot.send_message(i[2], """Над решениями работали:\nГуев Тимур\nГуссаова Рената\nГутнова Дзерасса\nПлиева Мадина\nТасоева Эсма\nЧшиев Аслан""")
+#             time.sleep(1)
+#             bot.bot.send_message(i[2], "АНТИХАЙП")
+#             bot.log.info("{} ({}) : Доставлено".format(i[1],i[2]))
+#             time.sleep(2)
+#         except bot.telebot.apihelper.ApiException as ex:
+#             bot.log.error("{} ({}) : {}".format(i[1], i[2], ex))
 
-sendToAll("""Мы тут добавили разбор 202 варианта с сайта alexlarin.net. Он прям свежий.""")
+while(1):
+    bot.bot.send_message(115825833, text=input())
